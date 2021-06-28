5ccaa93c8f0e87a0ab7b7a66ffdfae4c0c4afc75
gari8 <hagari.hayato.infratop@gmail.com>
2021-06-28 21:09:47 +0900

    fix: change

--- src/modules/ApolloModule.tsx
+++ src/modules/ApolloModule.tsx
@@ -3,16 +3,16 @@ import { setContext } from '@apollo/client/link/context';
 import { tokenName } from '../contexts/Auth'
 
 const productionURL = "https://faves4.herokuapp.com/query"
-const developmentURL = "https://faves4-stg.herokuapp.com/query"
-const localURL = "http://localhost:8080/query"
+const previewURL = "https://faves4-stg.herokuapp.com/query"
+const developmentURL = "http://localhost:8080/query"
 
 const handleUri = (): string => {
     if (process.env.REACT_APP_DEPLOY_ENV === "production") {
         return productionURL
-    } else if (process.env.REACT_APP_DEPLOY_ENV === "development") {
-        return developmentURL
+    } else if (process.env.REACT_APP_DEPLOY_ENV === "preview") {
+        return previewURL
     } else {
-        return localURL
+        return developmentURL
     }
 }
 
