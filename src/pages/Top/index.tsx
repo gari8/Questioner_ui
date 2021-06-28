b7ff6f26a74af4b3e08cb8427a9b2466295066c6
gari8 <hagari.hayato.infratop@gmail.com>
2021-06-28 20:56:33 +0900

    test: env

--- src/pages/Top/index.tsx
+++ src/pages/Top/index.tsx
@@ -1,10 +1,12 @@
 import {FC} from "react";
 import {Link} from "react-router-dom";
+import { Text } from '@chakra-ui/react'
 
 const Top: FC = () => {
 	return (
 		<>
 			<Link to={"/question"}>Question</Link>
+			<Text>{process.env.DEPLOY_ENV ? process.env.DEPLOY_ENV : "local"}</Text>
 		</>
 	)
 }
